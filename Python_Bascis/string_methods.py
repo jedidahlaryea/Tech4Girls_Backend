6b85167bda35084587ab2e2943df6c6aaa32fea9
jedidahlaryea <jeddstudy@gmail.com>
2024-10-23 15:09:58 +0000

    Question3

---
+++ Python_Bascis/string_methods.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+little_string = 'This is fun'
+print(little_string.upper())
+print(little_string.lower())
+print(little_string.replace('fun', 'interesting'))
+
+name = 'Jedidah'
+comment = 'You are beautiful'
+print(name + ' ' + comment)
