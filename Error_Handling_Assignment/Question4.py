46d38e60e9515e131bfe1feac70c3142cdafb0b6
jedidahlaryea <jeddstudy@gmail.com>
2024-11-11 16:36:59 +0000

    Question4

--- Error_Handling_Assignment/Question4.py
+++ Error_Handling_Assignment/Question4.py
@@ -4,5 +4,4 @@ index = int(input("Enter the index of the number you want to access"))
 try:
     print("You selected:",items[index])
 except IndentationError:
-    print(Index'{index}'cannot be found on the list)
-    
+    print(f"Index'{index}'cannot be found on the list")
